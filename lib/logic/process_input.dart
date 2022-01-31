17fafafd039bd3f982f467970e1bb3a5a4daff0d
Shantanu Goel <shantanu.goel@gmail.com>
2022-01-31 22:37:33 +0530

    Basic emojification working

--- lib/logic/process_input.dart
+++ lib/logic/process_input.dart
@@ -1,9 +1,17 @@
-// import 'package:flutter_emoji/flutter_emoji.dart';
+import 'package:flutter_emoji/flutter_emoji.dart';
 import 'package:flutter/material.dart';
 
 ValueNotifier<String> inputChangeNotifier = ValueNotifier("");
 
 void processInput(String input) {
-  // var parser = EmojiParser();
-  inputChangeNotifier.value = input;
+  var parser = EmojiParser();
+  String output = "";
+  var tokens = input.split(" ");
+  for (var token in tokens) {
+    // Can also use synonyms? e.g. heart instead of love
+    var emoji = parser.get(token);
+    output += (emoji == Emoji.None) ? token : emoji.code;
+    output += " ";
+  }
+  inputChangeNotifier.value = output;
 }
