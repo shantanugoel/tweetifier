33a85da601950b41a131f540d6e325cd6a5ab5cb
Shantanu Goel <shantanu.goel@gmail.com>
2022-01-31 22:53:44 +0530

    replace flutter_emoji by emojis

--- lib/logic/process_input.dart
+++ lib/logic/process_input.dart
@@ -1,17 +1,25 @@
-import 'package:flutter_emoji/flutter_emoji.dart';
+import 'package:emojis/emoji.dart';
 import 'package:flutter/material.dart';
 
 ValueNotifier<String> inputChangeNotifier = ValueNotifier("");
 
 void processInput(String input) {
-  var parser = EmojiParser();
   String output = "";
   var tokens = input.split(" ");
   for (var token in tokens) {
     // Can also use synonyms? e.g. heart instead of love
-    var emoji = parser.get(token);
-    output += (emoji == Emoji.None) ? token : emoji.code;
+    final emoji = Emoji.byKeyword(token);
+    // output += (emoji == Emoji.None) ? token : emoji.code;
+    if (emoji != null) {
+      // Fix this condition. Also this is too strong. Maybe avoid doing for 1/2 characters
+      output += emoji.first.char;
+    } else {
+      output += token;
+    }
     output += " ";
   }
   inputChangeNotifier.value = output;
 }
+
+//use share_plus package for sharing
+// can also try emojis  or dart_emoji package
