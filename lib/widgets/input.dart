2be3c7421910abbce240110af967eb49e37bdbfd
Shantanu Goel <shantanu.goel@gmail.com>
2022-02-05 03:52:41 +0530

    Add clear button to input

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -11,11 +11,13 @@ class Input extends StatefulWidget {
 
 class _InputState extends State<Input> {
   int letterCount = 0;
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       keyboardType: TextInputType.multiline,
+      controller: _controller,
       minLines: 3,
       maxLines: 20,
       decoration: InputDecoration(
@@ -27,13 +29,24 @@ class _InputState extends State<Input> {
             color: Colors.blue,
           ),
         ),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () => _controller.clear(),
+        ),
       ),
-      onChanged: (String value) {
-        inputChangeNotifier.value = value;
-        setState(() {
-          letterCount = value.characters.length;
-        });
-      },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_inputChanged);
+  }
+
+  void _inputChanged() {
+    setState(() {
+      letterCount = _controller.text.characters.length;
+    });
+    inputChangeNotifier.value = _controller.text;
   }
 }
