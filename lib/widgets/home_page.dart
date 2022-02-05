e839a3cd846d8148f62609a6d2ea563edd11f69c
Shantanu Goel <shantanu.goel@gmail.com>
2022-02-05 19:16:15 +0530

    MVP

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tweetifier'),
+        title: const Text('Tweetify!'),
         actions: [
           IconButton(
             onPressed: () => showHelp(context),
